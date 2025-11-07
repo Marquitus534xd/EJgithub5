afab416a951285bb4cb4936c000c7ccc92a58f8a
arnau <25marc.cepeda@inscastellet.cat>
2025-11-07 17:52:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#EJgithub5 
---
+++ src/main/java/com/mycompany/ejgithub5/EJgithub5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejgithub5;
+
+/**
+ *
+ * @author Arnau
+ */
+public class EJgithub5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
