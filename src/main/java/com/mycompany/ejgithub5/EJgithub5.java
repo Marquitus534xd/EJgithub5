f10f2199fa72cfc46f0317cabd76dcb0d5c8cfa5
Marc Cepeda <marccepediosvibaldios@gmail.com>
2025-11-14 15:31:41 +0100

    Update EJgithub5.java

--- src/main/java/com/mycompany/ejgithub5/EJgithub5.java
+++ src/main/java/com/mycompany/ejgithub5/EJgithub5.java
@@ -4,13 +4,19 @@
 
 package com.mycompany.ejgithub5;
 
-/**
- *
- * @author Arnau
- */
-public class EJgithub5 {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class EJgithub5 {
+public static void main(String[] args) {
+        Scanner llegir = new Scanner(System.in);
+        System.out.println ("Digues un numero entre el 1 i el 10");     
+        System.out.println ("Introdueix un numero: ");
+        int numero;
+        int randomNum = (int)(Math.random() * 11);  // 0 to 10
+        do {
+        numero = llegir.nextInt();
+        System.out.println ("Has esollit el numero : "+ numero);
+        }while (!(numero == randomNum));
+        System.out.println ("Has encertat ole tu");
+} 
 }
